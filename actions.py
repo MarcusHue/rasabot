a4b92c810f4b7fba128a2f314d1d8672627167ca
MarcusHue <marcus@huelsdau.com>
2020-03-31 15:46:57 +0200

    dogs

--- actions.py
+++ actions.py
@@ -1,33 +1,54 @@
 from typing import Any, Text, Dict, List
-
 from rasa_sdk import Action, Tracker
 from rasa_sdk.executor import CollectingDispatcher
 import requests as re 
 import json
 import time
+import api_creds
 
-class TellChuckNorrisJoke(Action):
+class TellDadJoke(Action):
 
     def name(self) -> Text:
-        return "action_tell_chuck_norris_joke"
+        return "action_tell_dad_joke"
 
     def run(self, dispatcher: CollectingDispatcher,
             tracker: Tracker,
             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
 
-        joke = re.get('https://api.chucknorris.io/jokes/random').json()['value']
+        joke = re.get('https://icanhazdadjoke.com/', headers={"Accept":"application/json"}).json()['joke']
         dispatcher.utter_message(text=joke)
 
         return []
 
+class ShowDog(Action)
 
-class WaitOneSecond(Action):
-    
-    def name(self) -> Text: 
-        return "wait_one_second"
+    def name(self) -> Text:
+        return "action_show_dog"
 
-    def run(self,
+    def run(self, dispatcher: CollectingDispatcher,
             tracker: Tracker,
             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-        time.sleep(1)
-        return[]
+
+        search_string = tracker.get_slot('dog_breed')
+        search_breed_id_url = 'https://api.thedogapi.com/v1/breeds/search?q=' + search_string
+        dog = re.get(search_breed_id_url, headers={"x-api-key":ac.dog_key}).json()
+        if len(dog) > 1:
+            dogs_list = [x['name'] for x in dog]
+            buttons = [{'title':dog,'payload':'/fav_breed{"fav_breed":'+dog+"}"} for dog in dogs_list]
+            dispatcher.utter_button_message(text='Which breed do you mean?',buttons=buttons)
+        else if len(dog) == 1:
+            dog = dog[0]
+            dog_name = dog['name']
+            try:
+                dog_temp = dog['temperament'].split(",")
+                dog_temp = [x.strip(' ') for x in dog_temp]
+            except: 
+                dog_temp = []
+            search_img_breed_id_url = 'https://api.thedogapi.com/v1/images/search?breed_id=' + str(dog['id'])
+            dog_pic_json = re.get(search_img_breed_id_url, headers={"x-api-key":ac.dog_key}).json()
+            if dog_pic_json:
+                dog_pic_url = dog_pic_json[0]['url']
+        else:
+            dog = []
+
+        return []
--- sandbox.py
+++ sandbox.py
@@ -1,7 +1,29 @@
 import requests as re 
 import json
+import api_creds as ac
 
-joke = re.get('https://api.chucknorris.io/jokes/random').json()['value']
 
 
-print(joke)
+
+search_string = "pood"
+search_breed_id_url = 'https://api.thedogapi.com/v1/breeds/search?q=' + search_string
+dog = re.get(search_breed_id_url, headers={"x-api-key":ac.dog_key}).json()
+if len(dog) > 1:
+    dogs_list = [x['name'] for x in dog]
+    buttons = [{'title':dog,'payload':'/fav_breed{"fav_breed":'+dog+"}"} for dog in dogs_list]
+elif len(dog) == 1:
+    dog = dog[0]
+    dog_name = dog['name']
+    try:
+        dog_temp = dog['temperament'].split(",")
+        dog_temp = [x.strip(' ') for x in dog_temp]
+    except: 
+        dog_temp = []
+    search_img_breed_id_url = 'https://api.thedogapi.com/v1/images/search?breed_id=' + str(dog['id'])
+    dog_pic_json = re.get(search_img_breed_id_url, headers={"x-api-key":ac.dog_key}).json()
+    if dog_pic_json:
+        dog_pic_url = dog_pic_json[0]['url']
+else:
+    dogs = ''
+
+print('bla')
